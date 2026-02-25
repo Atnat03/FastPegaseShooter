0cd86f6f26281ca31bdafca524df70b458f5976c
Limpin <limpin01.23@gmail.com>
2026-02-25 14:37:47 +0100

    Event Bus

---
+++ Assets/Scripts/Managers/Bus/EventBusInitialiser.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EventBusInitialiser : MonoBehaviour
+{
+    private void Awake()
+    {
+        //Bus Creation
+        EventBus bus = new EventBus();
+        
+        
+    }
+}
