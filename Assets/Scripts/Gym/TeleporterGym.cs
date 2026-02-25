1f59956fbcd4f4749f7d337930d936a058550ac7
EliottLezin <159541334+EliottLezin@users.noreply.github.com>
2026-02-25 22:31:47 +0100

    Suite GymMovement + Test Size arène
    Test de différentes tailles d'arène + tp entre les différentes zones de la gymMovement et début des ateliers de test de la partie Movement du controller. + crosshair et code autre cheap

---
+++ Assets/Scripts/Gym/TeleporterGym.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeleporterGym : MonoBehaviour
+{
+    [SerializeField] private Transform p_zoneToTp;
+    [SerializeField] private TextMeshProUGUI p_display;
+
+    private void Awake()
+    {
+        if(p_zoneToTp != null) p_display.text = p_zoneToTp.gameObject.name;
+    }
+        
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent.position = p_zoneToTp.position;
+    }
+}
