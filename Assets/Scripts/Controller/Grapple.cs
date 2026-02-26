4b728efe497b4a5e41f40744605b4286edfb8933
Abal <bergerbarnabe@gmail.com>
2026-02-26 16:18:43 +0100

    grapple pas tout a fait fonctionnel mais sans bug

--- Assets/Scripts/Controller/Grapple.cs
+++ Assets/Scripts/Controller/Grapple.cs
@@ -9,9 +9,13 @@ public class Grapple : NetworkBehaviour
     
     [Header("References")]
     [SerializeField] PlayerInput playerInput;
+    [SerializeField] Rigidbody rb;
     
     [Header("To tweak")]
-    [SerializeField] private float _castWidth = 3f;
+    [SerializeField] private float _castWidth = .5f;
+    [SerializeField] private float _castMaxDistance = 100f;
+    [SerializeField] private float _grapplingSpeed;
+    [SerializeField] private float _endGrappleImpulseForce = 3f;
     
     Transform _camTransform;
     private Transform _currentGrapplePoint ;
@@ -35,9 +39,9 @@ public class Grapple : NetworkBehaviour
     
     void CastGrapple(InputAction.CallbackContext ctx)
     {
-        if (Physics.SphereCast(_camTransform.position, _castWidth, _camTransform.forward, out RaycastHit hit))
+        if (Physics.SphereCast(_camTransform.position, _castWidth, _camTransform.forward, out RaycastHit hit, _castMaxDistance,LayerMask.GetMask("Default"),QueryTriggerInteraction.Collide))
         {
-            if (hit.collider.CompareTag("Player"))
+            if (hit.collider.CompareTag("GrapplePoint"))
             {
                 _currentGrapplePoint =  hit.collider.transform;
                 StartCoroutine(GrappleCoroutine());
@@ -50,7 +54,14 @@ public class Grapple : NetworkBehaviour
         while (Vector3.Distance(transform.position, _currentGrapplePoint.position) > 0.1f &&
                playerInput.actions["Grapple"].IsPressed())
         {
+            rb.MovePosition(Vector3.MoveTowards(transform.position, _currentGrapplePoint.position, _grapplingSpeed * Time.fixedDeltaTime));
             yield return new WaitForFixedUpdate();
+        }
+
+        if (playerInput.actions["Grapple"].IsPressed())
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.AddForce(Vector3.up * _endGrappleImpulseForce, ForceMode.Impulse);
         }
 
         _currentGrapplePoint = null;
