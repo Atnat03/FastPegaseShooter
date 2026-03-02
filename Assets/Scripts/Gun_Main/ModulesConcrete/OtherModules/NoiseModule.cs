f3f784fb612e175c80ef6793d5d9ee1c64f42100
BOURRICAUD Antoine <a.bourricaud@rubika-edu.com>
2026-03-02 11:01:54 +0100

    delete OnValidate

--- Assets/Scripts/Gun_Main/ModulesConcrete/OtherModules/NoiseModule.cs
+++ Assets/Scripts/Gun_Main/ModulesConcrete/OtherModules/NoiseModule.cs
@@ -9,15 +9,5 @@ namespace GunDecorator
         {
             Debug.Log("OMG ça se disperse !!");
         }
-        
-        //Empecher d'ajouter le component si il y a deja un autre component particulier 
-        private void OnValidate()
-        {
-            FragShootModule[] shootModules = GetComponents<FragShootModule>().ToArray();
-            if (shootModules.Length > 0)
-            {
-                Debug.LogError("Vous ne pouvez pas ajouté un module de --Noise-- car il y a déja un module de --FragShoot-- !");
-            }
-        }
     }
 }
