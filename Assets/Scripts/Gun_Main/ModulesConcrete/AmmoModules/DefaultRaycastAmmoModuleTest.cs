473be130af043ff0767152b65b23f4e58d9a9ed1
Abal <bergerbarnabe@gmail.com>
2026-03-05 15:58:58 +0100

    bullet Obstacle Detection RealTime

--- Assets/Scripts/Gun_Main/ModulesConcrete/AmmoModules/DefaultRaycastAmmoModuleTest.cs
+++ Assets/Scripts/Gun_Main/ModulesConcrete/AmmoModules/DefaultRaycastAmmoModuleTest.cs
@@ -9,7 +9,7 @@ namespace GunDecorator.AmmoModules
         
         [Header("references")]
         [SerializeField] private Camera _camera;
-        [SerializeField] private GameObject _visualBulletPrefab;
+        [SerializeField] private GameObject BulletPrefab;
 
         [Header("parametres")]
         [SerializeField] private float _maxDistance;
@@ -18,7 +18,6 @@ namespace GunDecorator.AmmoModules
         
         [Header("Debug")]
         public GameObject p_markPrefab;
-        private GameObject _currentMark;
         
         //privates
         private Transform _camTransform;
@@ -31,43 +30,27 @@ namespace GunDecorator.AmmoModules
         }
 
 
-        private Vector3 _spawnPos;
+        private Vector3 bulletDirection;
+        private float travelTime;
         public void SpawnBullet()
         {
-            _spawnPos = _camTransform.position + transform.forward * .3f;
-            if (Physics.Raycast(_spawnPos, _camTransform.forward, out RaycastHit hit,_maxDistance, ~LayerMask.GetMask("Owner")))
+            if (Physics.Raycast(_camTransform.position + transform.forward * .3f, _camTransform.forward, out RaycastHit hit,_maxDistance, ~LayerMask.GetMask("Owner")))
             {
-                if (_currentMark != null)
-                {
-                    Destroy(_currentMark);
-                }
-                
-                float travelTime = hit.distance / _BulletSpeed;
-                
-                StartCoroutine(TravelTimeCoroutine(hit, travelTime, Instantiate(_visualBulletPrefab,transform.position + transform.forward * .3f, Quaternion.identity), _spawnPos));
+                bulletDirection = (hit.point - (_camTransform.position + transform.forward * .3f)).normalized;
+                travelTime = hit.distance / _BulletSpeed;
             }
-        }
-
-        IEnumerator TravelTimeCoroutine(RaycastHit hit, float travelTime, GameObject bullet, Vector3 spawnPos)
-        {
-            float elapsedTime = 0;
-            while (elapsedTime < travelTime)
+            else
             {
-                elapsedTime += Time.deltaTime;
-                bullet.transform.position += (hit.point - spawnPos).normalized * (_BulletSpeed * Time.deltaTime);
-                yield  return null;
+                bulletDirection = _camera.transform.forward;
+                travelTime = _maxDistance /  _BulletSpeed;
             }
-            Destroy(bullet);
-            HitTarget(hit);
-        }
-
-        private void HitTarget(RaycastHit hit)
-        {
-            _currentMark = Instantiate(p_markPrefab, hit.point + hit.normal * 0.1f, Quaternion.LookRotation(hit.normal));
-            if (hit.collider.TryGetComponent<IDamagable>(out IDamagable iDamagable))
-            {
-                iDamagable.TakeDamage((int)_damages);
-            }
+            
+            GameObject newBullet = Instantiate(BulletPrefab, _camTransform.position + transform.forward * .3f, Quaternion.LookRotation(bulletDirection));
+            Destroy(newBullet, travelTime + .5f);
+            BulletBehaviour bulletBehaviour = newBullet.GetComponent<BulletBehaviour>();
+            bulletBehaviour.p_damage =  _damages;
+            bulletBehaviour.p_speed =  _BulletSpeed;
+            bulletBehaviour.p_markPrefab =  p_markPrefab;
         }
 
         public void SetDamage(float multiplierDmg)
