660e5c4d603ff58dc25dda08463232920dc684d5
a.bourricaud <antoine.bourricaud@gmail.com>
2026-04-28 15:27:41 +0200

    Angela hsls...

--- Assets/Placeholders/Shader/HLSL/GetShadowPass.hlsl
+++ Assets/Placeholders/Shader/HLSL/GetShadowPass.hlsl
@@ -2,9 +2,10 @@
 #define SHADOW_UTILS_INCLUDED
 
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Shadows.hlsl"
 
-void GetMainLightShadow_float(float3 PositionWS, out float Shadow)
+void GetMainLightShadow(float3 PositionWS, out float Shadow)
 {
     float4 shadowCoord = TransformWorldToShadowCoord(PositionWS);
     Shadow = MainLightRealtimeShadow(shadowCoord);
