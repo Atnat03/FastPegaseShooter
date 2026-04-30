13bd89ca137cf3fbce37052446d299c398d7c34a
a.bourricaud <antoine.bourricaud@gmail.com>
2026-04-30 10:42:03 +0200

    On voit la vie de bro

---
+++ Assets/Scripts/Controller/PlayerHealthManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using MyPrint;
+using UnityEngine;
+
+public class PlayerHealthManager : NetworkBusListener
+{
+	public static PlayerHealthManager Instance { get; private set; }
+
+	private readonly List<PlayerHealth> _registeredPlayers = new();
+
+	public IReadOnlyList<PlayerHealth> RegisteredPlayers => _registeredPlayers;
+
+	public Action OnRegistryUpdated;
+
+	private void Awake()
+	{
+		Instance = this;
+	}
+
+	public void Register(PlayerHealth playerHealth)
+	{
+		if (!_registeredPlayers.Contains(playerHealth))
+		{
+			Cons.Print("Register", ColorConsole.Orange);
+			_registeredPlayers.Add(playerHealth);
+			OnRegistryUpdated?.Invoke();
+		}
+	}
+
+	public void Unregister(PlayerHealth playerHealth)
+	{
+		_registeredPlayers.Remove(playerHealth);
+		OnRegistryUpdated?.Invoke();
+	}
+}
+
+public struct PlayerHealthRegisteredEvent
+{
+	public PlayerHealth p_playerHealth;
+}
+
+public struct PlayerHealthUnregisteredEvent
+{
+	public PlayerHealth p_playerHealth;
+}
