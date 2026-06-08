b84ccea703531da77fadb118d5b4fdb24edcaf8b
Abal <bergerbarnabe@gmail.com>
2026-06-08 15:29:47 +0200

    Debug respawn button

--- Assets/Scripts/Debug/DebugPlayerOptions.cs
+++ Assets/Scripts/Debug/DebugPlayerOptions.cs
@@ -5,10 +5,23 @@ public class DebugPlayerOptions : MonoBehaviour
 {
     [SerializeField] PlayerInput _playerInput;
     [SerializeField] FPSController _fpsController;
+    [SerializeField] PlayerHealth _playerHealth;
 
-    void Start()
+    void OnEnable()
     {
         _playerInput.actions["DebugLeaveGrapple"].performed += ControllerLeaveState;
+        _playerInput.actions["DebugRespawn"].performed += Respawn;
+    }
+
+    void OnDisable()
+    {
+        _playerInput.actions["DebugLeaveGrapple"].performed -= ControllerLeaveState;
+        _playerInput.actions["DebugRespawn"].performed -= Respawn;
+    }
+
+    private void Respawn(InputAction.CallbackContext obj)
+    {
+        _playerHealth.Respawn();
     }
 
     private void ControllerLeaveState(InputAction.CallbackContext obj)
