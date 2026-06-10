df163c07669555e5893b19237281b6ff0d4798a3
Abal <bergerbarnabe@gmail.com>
2026-06-10 09:28:57 +0200

    debug fonctiuns fix

--- Assets/Scripts/Controller/PlayerVisuelBridge.cs
+++ Assets/Scripts/Controller/PlayerVisuelBridge.cs
@@ -3,18 +3,6 @@ using UnityEngine;
 
 public class PlayerVisuelBridge : NetworkBehaviour
 {
-	#region Properties
-
-	public PlayerHealth PlayerHealth => _playerHealth;
-	public GunSwitching PlayerGun => _playerGun;
-	public PlayerDroneView PlayerDroneView => _playerDroneView;
-	
-	public FPSController FPSController => _fpsController;
-	public PlayerPositionCaster PlayerPositionCaster => _playerPositionCaster;
-	
-	#endregion
-
-
 	#region Variables
 
 	[SerializeField] private PlayerHealth _playerHealth;
@@ -22,13 +10,6 @@ public class PlayerVisuelBridge : NetworkBehaviour
 	[SerializeField] private PlayerDroneView _playerDroneView;
 	[SerializeField] private FPSController _fpsController;
 	[SerializeField] private PlayerPositionCaster _playerPositionCaster;
-	
-	#endregion
-
-
-	#region Fonctions
-
-	
 	
 	#endregion
 }
