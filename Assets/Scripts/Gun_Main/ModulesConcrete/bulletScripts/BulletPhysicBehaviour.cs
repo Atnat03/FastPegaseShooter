2529a18bf554b676c4d516002b0b6cdedda9d744
a.bourricaud <antoine.bourricaud@gmail.com>
2026-06-12 16:16:00 +0200

    fix sound explosion

--- Assets/Scripts/Gun_Main/ModulesConcrete/bulletScripts/BulletPhysicBehaviour.cs
+++ Assets/Scripts/Gun_Main/ModulesConcrete/bulletScripts/BulletPhysicBehaviour.cs
@@ -87,7 +87,7 @@ public class BulletPhysicBehaviour : MonoBusListener, IAmmo, IPoolable
     {
         if (_vfx != null)
         {
-            SoundManager.PlaySound(_dataSound, "Explosion", _audioSource);
+            SoundManager.PlaySound(_dataSound, "Explosion", _audioSource, _gunController.transform);
             
             GameObject v = Instantiate(_vfx);
             v.transform.position = transform.position + transform.up * 0.1f;
