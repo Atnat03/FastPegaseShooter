34d03d5870bb56b4579071091f08801689efd391
Abal <bergerbarnabe@gmail.com>
2026-06-13 13:03:40 +0200

    dialogues class struc fix

--- Assets/Scripts/Dialogues/DialogueStarterUseDrone.cs
+++ Assets/Scripts/Dialogues/DialogueStarterUseDrone.cs
@@ -1,10 +1,10 @@
 
-public struct OnUseDrone_Dialogue
-{
-    public bool isPositive;
-}
-
 public class DialogueStarterUseDrone : DialogueStarterGeneric<OnUseDrone_Dialogue>
 {
     protected override void OnEventTriggered(OnUseDrone_Dialogue e) => PlayDialogue(e.isPositive);
+}
+
+public struct OnUseDrone_Dialogue
+{
+    public bool isPositive;
 }
