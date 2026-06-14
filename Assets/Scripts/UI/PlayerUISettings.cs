19460122f95d6218f170ca67a97fdc468a74f9e9
a.bourricaud <antoine.bourricaud@gmail.com>
2026-06-14 14:37:41 +0200

    add musique + loading ui

--- Assets/Scripts/UI/PlayerUISettings.cs
+++ Assets/Scripts/UI/PlayerUISettings.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using FishNet;
+using FishNet.Managing.Scened;
 using ScriptableObjectsDefinitions;
 using UnityEngine;
 using UnityEngine.UI;
@@ -25,13 +28,21 @@ public class PlayerUISettings : NetworkBusListener
 
 	#region Variables
 
+	[SerializeField] private Canvas _canvas;
+	
 	[Header("Sound")]
 	[SerializeField] private SoundsDataSO _soundsData;
 	[SerializeField] private AudioSource _source;
 	
+	[Header("LoadingScene")]
+	[SerializeField] private GameObject _loadingSceneUI;
+	
 	[Header("Colors")]
 	[SerializeField] private GunSwitching _gunSwitching;
 	[SerializeField] private ColorToChange[] _imageToChangeList;
+
+	private int baseSortingCanvaLayer;
+	private bool _isLoading;
 	
 	#endregion
 
@@ -42,6 +53,24 @@ public class PlayerUISettings : NetworkBusListener
 	{
 		ListenToEvent<PlayUISound>(PlaySoundUI);
 		ListenToEvent<OnPlayerOk>(ApplyColor);
+
+		baseSortingCanvaLayer = _canvas.sortingOrder;
+		
+		ListenToEvent<OnShowLoadingScreen>(_ =>
+		{
+			_isLoading = true;
+			_canvas.sortingOrder = 10;
+			_loadingSceneUI.SetActive(true);
+		});
+
+
+		InstanceFinder.SceneManager.OnLoadEnd += OnLoadEnd;
+	}
+	
+	public override void OnStopNetwork()
+	{
+		if (InstanceFinder.SceneManager != null)
+			InstanceFinder.SceneManager.OnLoadEnd -= OnLoadEnd;
 	}
 
 	private void PlaySoundUI(PlayUISound data)
@@ -77,6 +106,20 @@ public class PlayerUISettings : NetworkBusListener
 		{
 			data.image.color = playerData.IsPositive ? data.colorPositive : data.colorNegative;
 		}
+	}
+	
+	private void OnLoadEnd(SceneLoadEndEventArgs args)
+	{
+		StartCoroutine(HideLoading());
+	}
+
+	private IEnumerator HideLoading()
+	{
+		yield return null;
+
+		_loadingSceneUI.SetActive(false);
+		_canvas.sortingOrder = baseSortingCanvaLayer;
+		_isLoading = false;
 	}
 
 	#endregion
