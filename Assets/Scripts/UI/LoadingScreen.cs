d1b2cf9b688820b6ac33c9f1d65cd9ba5fdd5bfa
a.bourricaud <antoine.bourricaud@gmail.com>
2026-06-15 14:34:35 +0200

    fix loading from connection

--- Assets/Scripts/UI/LoadingScreen.cs
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Threading.Tasks;
 using FishNet;
 using FishNet.Managing.Scened;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LoadingScreen : MonoBusListener
@@ -10,6 +11,25 @@ public class LoadingScreen : MonoBusListener
     [SerializeField] private GameObject loadingScreen;
     [SerializeField] private float _minimumLoadingTime = 1;
 
+    private bool fromConnection = true;
+    
+    void Awake()
+    {
+        if (fromConnection)
+        {
+            StartCoroutine(ConnectionLoading());
+        }
+    }
+
+    IEnumerator ConnectionLoading()
+    {
+        loadingScreen.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        loadingScreen.SetActive(false);
+
+        fromConnection = false;
+    }
+    
     private void OnEnable()
     {
         InstanceFinder.SceneManager.OnLoadStart += OnLoadStart;
